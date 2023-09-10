0d98ec51729cf71790d3c01ccfe93b56fd0e7487
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-09-10 12:22:46 +0200

    feat: update aws objects

--- controllers/project.js
+++ controllers/project.js
@@ -103,22 +103,56 @@ exports.createProject = async (req,res,next) => {
 exports.updateProject = async (req, res, next) => {
     let projectObject = null
     let newContent = null
+    let project = await Project.findOne({_id: req.params.id})
 
     if(req.file) {
         let name = req.file.originalname.split(' ').join('_') //Construct a new name for the file
-        let completeName = Date.now() + name
+        let imageName = Date.now() + name
+        let smallImageName = "small-"+imageName
         let image = sharp(req.file.buffer)
-        
+
+        let oldName = project.imageUrl.split('.com/')[1]
+
         projectObject = {
             ...JSON.parse(req.body.project),
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${completeName}`
+            imageUrl: `https://portfolio-ac-public.s3.eu-west-3.amazonaws.com/${imageName}`
         }
         newContent = {
             language: projectObject.content[0].language,
             text: projectObject.content[0].text
         }
-        await image.toFile(`./images/${completeName}`)
-        await image.resize(450,null).toFile(`./images/small/${completeName}`)
+        let bucketName = process.env.BUCKET_NAME
+
+        let params1 = {
+            Bucket: bucketName,
+            Key: imageName,
+            Body: req.file.buffer,
+            ContentType: req.file.mimetype
+        }
+
+        let smallImage = await image.resize(315,null).toBuffer()
+        let params2 = {
+            Bucket: bucketName,
+            Key: smallImageName,
+            Body: smallImage,
+            ContentType: req.file.mimetype
+        }
+
+        let command1 = new PutObjectCommand(params1)
+        let command2= new PutObjectCommand(params2)
+        let delete1 = new DeleteObjectCommand({
+            Bucket: bucketName,
+            Key: oldName
+        })
+        let delete2 = new DeleteObjectCommand({
+            Bucket: bucketName,
+            Key: "small-"+oldName
+        })
+
+        await s3.send(command1)
+        await s3.send(command2)
+        await s3.send(delete1)
+        await s3.send(delete2)
     } else {
         projectObject = { ...req.body }
         newContent = {
@@ -129,41 +163,37 @@ exports.updateProject = async (req, res, next) => {
 
     delete projectObject.content
     delete projectObject.userId
-
-    Project.findOne({_id: req.params.id})
-        .then(project => {
-            if (project.userId !== req.auth.userId) {
-                res.status(403).json({ message : 'unauthorized request'})
-            } else {
-                if (project.content.some((input) => input.language === newContent.language)) {
-                    Project.updateOne({_id: req.params.id, 'content.language': newContent.language},
-                        {
-                            ...projectObject,
-                            userId: req.auth.userId,
-                            _id: req.params.id,
-                            $set: {
-                                'content.$.text' : newContent.text
-                            }
-                        },
-                        )
-                        .then(() =>  res.status(201).json({ message: 'Projet modifié'}))
-                        .catch(error => res.status(400).json({ error }))
-                } else {
-                    Project.updateOne({_id: req.params.id},
-                        {
-                            ...projectObject,
-                            userId: req.auth.userId,
-                            id: req.params.id,
-                            $push: {
-                                content: {language: newContent.language, text: newContent.text }
-                            }
-                        })
-                        .then(() =>  res.status(201).json({ message: 'Projet modifié'}))
-                        .catch(error => res.status(400).json({ error }))
-                }
-            }
-        })
-        .catch(error => res.status(400).json({ error }))
+     
+    if (project.userId !== req.auth.userId) {
+        res.status(403).json({ message : 'unauthorized request'})
+    } else {
+        if (project.content.some((input) => input.language === newContent.language)) {
+            Project.updateOne({_id: req.params.id, 'content.language': newContent.language},
+                {
+                    ...projectObject,
+                    userId: req.auth.userId,
+                    _id: req.params.id,
+                    $set: {
+                        'content.$.text' : newContent.text
+                    }
+                },
+                )
+                .then(() =>  res.status(201).json({ message: 'Projet modifié'}))
+                .catch(error => res.status(400).json({ error }))
+        } else {
+            Project.updateOne({_id: req.params.id},
+                {
+                    ...projectObject,
+                    userId: req.auth.userId,
+                    id: req.params.id,
+                    $push: {
+                        content: {language: newContent.language, text: newContent.text }
+                    }
+                })
+                .then(() =>  res.status(201).json({ message: 'Projet modifié'}))
+                .catch(error => res.status(400).json({ error }))
+        }
+    }
 }
 
 //Delete one project
--- controllers/video.js
+++ controllers/video.js
@@ -30,29 +30,30 @@ exports.createVideo = async (req,res,next) => {
 //Modify a video
 exports.updateVideo = async (req, res, next) => {
     let newVideo = JSON.parse(req.body.content)
-    
+    let video = await Video.findOne({projectId: req.params.id})
     delete newVideo.userId
 
-    Video.findOne({projectId: req.params.id})
-    .then(video => {
-        if (video.userId !== req.auth.userId) {
-            res.status(403).json({ message : 'unauthorized request'})
-        } else {
-            let filename = video.videoUrl.split('/videos/')[1]
-            fs.unlink(`videos/${filename}`, () => {
-                Video.updateOne({projectId: req.params.id},
-                    {
-                        ...newVideo,
-                        videoUrl:  `${req.protocol}://${req.get('host')}/videos/${req.file.filename}`,
-                        userId: req.auth.userId,
-                    }
-                )
-                .then(() => res.status(201).json({ message: 'Vidéo enregistrée'}))
-                .catch(error => res.status(400).json({ error }))
-            })
-        }
-    })
-    .catch(error => res.status(400).json({ error }))
+    if (video.userId !== req.auth.userId) {
+        res.status(403).json({ message : 'unauthorized request'})
+    } else {
+        let filename = video.videoUrl.split('.com/')[1]
+        let deleteImage = new DeleteObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Key: filename
+        })
+        
+        await s3.send(deleteImage)
+
+        Video.updateOne({projectId: req.params.id},
+            {
+                ...newVideo,
+                videoUrl: req.file.location,
+                userId: req.auth.userId,
+            }
+        )
+        .then(() => res.status(201).json({ message: 'Vidéo enregistrée'}))
+        .catch(error => res.status(400).json({ error }))
+    }
 }
 
 //Delete one video
